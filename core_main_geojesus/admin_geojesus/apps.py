0b9c98aa751e341689f00f3b631367966e745509
Jesus <jesus.romerocaballero@cesunbc.edu.mx>
2026-02-24 01:27:14 -0800

    Add files via upload

---
+++ core_main_geojesus/admin_geojesus/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdminGeojesusConfig(AppConfig):
+    name = 'admin_geojesus'
---
+++ core_main_geojesus/admin_geojesus/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    context = {
+        "message": "- Hola a todos"
+    }
+    return render(request, 'index.html', context)
